b8b545be79cdd98e12a464781c04716259fedabb
Hinamizawa <themoonbunnys@gmail.com>
2022-02-01 12:13:32 -0400

    Create main.py
    Upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+first_name = "Kelvin"
+num = 22
+human = True
+first_name, num, human = "Kelvin", 22, True
+#print(first_name)
+print ("Your name is: " + (first_name))
+print ("Your age is: " + str(num))
+print ("Are you a human? " + str(human))
+#print(type(first_name))
