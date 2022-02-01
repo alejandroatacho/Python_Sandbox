14eac352eebbe9d36252018ab1291c4697a781db
Hinamizawa <themoonbunnys@gmail.com>
2022-02-01 15:51:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ first_name = "Kelvin"
 num = 22
 human = True
 first_name, num, human = "Kelvin", 22, True
+list =[5, 9, 12, 15, 20]
 #print(first_name)
 print ("Your name is: " + (first_name))
 print ("Your age is: " + str(num))
 print ("Are you a human? " + str(human))
+print (list)
 #print(type(first_name))
