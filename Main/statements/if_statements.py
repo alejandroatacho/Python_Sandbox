0d847b71af97e8fffff31f7cb380131b49ed9247
Hinamizawa <themoonbunnys@gmail.com>
2022-02-02 15:42:36 -0400

    sandboxboxbox

---
+++ Main/statements/if_statements.py
@@ -0,0 +1,10 @@
+age = int(input("How old are you?: "))
+
+if age >= 100:
+    print("you are old!")
+elif age >= 18:
+    print("your an adult!")
+elif age == 100:
+    print("damn for real?!?!?!")
+else:
+    print("Get out of here!!!!")
---
+++ Main/statements/logical_operators.py
@@ -0,0 +1,9 @@
+temp = int(input("what is the temperature outside?: "))
+
+if temp >= 0 and temp <= 30:
+    print("The temperature is good today!")
+    print("Go outside <3")
+elif temp < 0 or temp > 30:
+    print("stay inside!")
+'''else:
+    print("stay inside >w<!")'''
