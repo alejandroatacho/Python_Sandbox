7b7744099a7f20b051391221ca4631691352f029
Hinamizawa <themoonbunnys@gmail.com>
2022-02-02 09:47:41 -0400

    Create input.py

---
+++ python_sandbox/input.py
@@ -0,0 +1,7 @@
+name = input("what is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall are you?(cm): "))
+
+print("Hello " + name)
+print("You are "+str(age)+" years old")
+print("You are "+str(height)+" cm tall")
