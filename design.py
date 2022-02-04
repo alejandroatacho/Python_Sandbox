380e2d167dcfef57203b0579d3c8e627b19dca4e
Hinamizawa <themoonbunnys@gmail.com>
2022-02-04 14:04:30 -0400

    Create design.py

---
+++ design.py
@@ -0,0 +1,7 @@
+# include <windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, "Goodbye, cruel world!", "Note", MB_OK)
+    return 0
+}
