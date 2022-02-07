eb6038df5ddac9845126f673901ca64a75a875e6
Hinamizawa <themoonbunnys@gmail.com>
2022-02-07 10:11:16 -0400

    update

--- Main/functions/functions.py
+++ Main/functions/functions.py
@@ -3,9 +3,19 @@
 from unicodedata import name
 
 
-def hello(name=input("What is your name?: ")):
+'''def hello(name=input("What is your name?: ")):
     print("hello! " + name.capitalize() +
           " welcome to this python sandbox project")
 
 
 hello()
+'''
+
+
+def hello(first_name, last_name, age):
+    print("hello " + first_name+" "+last_name)
+    print("You are " + str(age) + " years old")
+    print("Have a nice day")
+
+
+hello("Bro", "Code", 21)
---
+++ Main/functions/return_statements.py
@@ -0,0 +1,10 @@
+'''return statement =functions send python values/objects back to the caller
+                    these values/objects are known as the function's return value'''
+
+
+def multiply(number1, number2):
+    result = number1 * number2
+    return result
+
+
+print(multiply(6, 8))
