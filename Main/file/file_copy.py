8d08ea0489d72f79c40376f3c5d03eaf55c9bf21
Hinamizawa <themoonbunnys@gmail.com>
2022-02-14 14:27:48 -0400

    update

--- Main/file/file_copy.py
+++ Main/file/file_copy.py
@@ -1,1 +1,7 @@
+# copyfile()= copies content of a file
+# copy() = copyfile() + permission mode + destination can be a directory
+# copy() + copies metadata (file's creation and modifaction times)
 import shutil
+
+shutil.copyfile("main\\file\\placeholder\\test.txt",
+                "main\\file\\placeholder\\copy_test.txt")
