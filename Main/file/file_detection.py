8e7979a39e29f78b350b60bc99fcf4fc9e164f67
Hinamizawa <themoonbunnys@gmail.com>
2022-02-14 11:07:14 -0400

    update

---
+++ Main/file/file_detection.py
@@ -0,0 +1,10 @@
+import os
+
+#path = "C:\\Users\\alex_\\Documents\GitHub\\Python_Sandbox\\Main\\file\\placeholder\\test.txt"
+# path = "main\\file\\placeholder\\test.txt"  # better version
+path = "main//file//placeholder//test.txt"  # Cleaner version
+if os.path.exists(path):
+    print("that location exists!")
+
+else:
+    print("that location does not exist")
--- Main/file/random_numbers.py
+++ Main/file/random_numbers.py
@@ -4,6 +4,7 @@ x = random.randint(1, 6)
 y = random.random()
 myList = ['rock', 'paper', 'scissors']
 z = random.choice(myList)
-print(x)
-print(y)
-print(z)
+
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 'Q', 'W', 'A']
+random.shuffle(cards)
+print(cards)
