f3e01f6322199a7d6059579815e5b0aa4f7862a2
Hinamizawa <themoonbunnys@gmail.com>
2022-02-23 13:48:34 -0400

    update

---
+++ Main/OOP/Class/classess.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def drive(self):
+        print("This " + self.model + " is driving")
+
+    def stop(self):
+        print("This " + self.model + "car has stopped")
---
+++ Main/OOP/Class/main.py
@@ -0,0 +1,7 @@
+from classess import Car
+
+car_1 = Car("Chevy", "Corvette", 2021, "blue")
+car_2 = Car("Ford", "Mustang", 2022, "Red")
+print(car_1.make)
+
+car_1.drive()
