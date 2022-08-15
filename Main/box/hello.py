d8fa3faf92ee09ee5e92c517a773192e603e7b5c
Hinamizawa <hinamizawa.github@gmail.com>
2022-08-15 18:03:10 -0400

    u

---
+++ Main/box/hello.py
@@ -0,0 +1,10 @@
+
+def display_code():
+    a = 1
+    b = 1
+    c = 10
+    for i in range(b, c + 1):
+        print(i)
+
+
+display_code()
---
+++ Main/functions/str_format.py
@@ -0,0 +1,14 @@
+animal = "cow"
+item = "grass"
+number = 3.14159265359
+number2 = 1000
+
+print("The {} ate the {}".format(animal, item))
+print("Hello, my name is {:10}".format(animal))
+print("Hello, my name is {:<10}".format(animal))
+print("Hello, my name is {:>10}".format(animal))
+print("Hello, my name is {:^10}".format(animal))
+print("the number of pi is {:.3f}".format(number))
+print("the number of pi is {:b}".format(number2))
+
+# https://youtu.be/XKHEtdqhLK8?t=9170
