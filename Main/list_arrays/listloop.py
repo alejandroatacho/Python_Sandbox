3ba517ccde1de6bbb430e63d682e8643139d3e18
Hinamizawa <hinamizawa.github@gmail.com>
2022-08-27 14:07:26 -0400

    Update listloop.py

--- Main/list_arrays/listloop.py
+++ Main/list_arrays/listloop.py
@@ -7,14 +7,13 @@ item_holder = []
 end_line = 5
 while i != end_line:
     item_log = input("Enter a list of items: ")
-    list_log = item_log.split(',')
-    list_log = item_holder + list_log
+    item_holder = item_log.split(',')
     print(item_log)
     i += 1
 
     if i == end_line:
         print("you have reached the end of the list")
-        print(list_log)
+        print(item_holder[i-1])
         break
     else:
         print("you have not reached the end of the list")
