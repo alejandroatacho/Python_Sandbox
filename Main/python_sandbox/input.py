0eac66d686d17ff59e88424fcd6e3e0f34d49ef1
Hinamizawa <hinamizawa.github@gmail.com>
2022-08-31 11:52:03 -0400

    up

--- Main/python_sandbox/input.py
+++ Main/python_sandbox/input.py
@@ -1,7 +1,19 @@
-name = input("what is your name?: ")
-age = int(input("How old are you?: "))
-height = float(input("How tall are you?(cm): "))
+# name = input("what is your name?: ")
+# age = int(input("How old are you?: "))
+# height = float(input("How tall are you?(cm): "))
 
-print("Hello " + name)
-print("You are "+str(age)+" years old")
-print("You are "+str(height)+" cm tall")
+# print("Hello " + name)
+# print("You are "+str(age)+" years old")
+# print("You are "+str(height)+" cm tall")
+
+from pynput.keyboard import Key, Listener
+
+key = []
+
+
+while key != Key.esc:
+    answer = input("Write your reply here: ")
+    print(answer + "\n" + "\n Write your reply here: ")
+    answer = input()
+    if key == Key.esc:
+        break
