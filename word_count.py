aee907b77aec7f4a003d4c6b97dbc6f013c8c713
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-16 16:45:41 -0400

    u

---
+++ word_count.py
@@ -0,0 +1,18 @@
+word_count = {}
+
+with open("text.txt", "r") as f:
+
+    def function_1():
+        counter = 0
+        for line in f:
+            words = line.split()
+            for word in words:
+                if word in word_count:
+                    word_count[word] += 1
+                else:
+                    word_count[word] = 1
+            counter += 1
+        print(f"There are {counter} lines.")
+        print(f"There are {len(word_count)} unique words in total in this file.")
+        
+    function_1()
